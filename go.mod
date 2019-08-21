bac589a7fb506deb91cd835dd152516d9445d048
Tim Satke <tim.satke@gmail.com>
2019-08-21 19:50:08 +0200

    Implemented getPointers

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.12
 
 require (
 	github.com/antlr/antlr4 v0.0.0-20190503160947-7c334b114c20
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/dolmen-go/endian v1.0.0
 	github.com/google/uuid v1.1.1
 	github.com/rs/zerolog v1.15.0
 	github.com/stretchr/testify v1.4.0
+	github.com/virtao/GoEndian v0.0.0-20140331034613-586fa83c856a
 )
--- tools/snapshot/snapshot.go
+++ tools/snapshot/snapshot.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"reflect"
 	"unsafe"
+
+	endian "github.com/virtao/GoEndian"
 )
 
 func Load(bytes []byte, v interface{}, size uintptr) error {
@@ -17,7 +19,10 @@ func Load(bytes []byte, v interface{}, size uintptr) error {
 }
 
 func Store(w io.Writer, v interface{}) error {
-	data := toBytes(v)
+	val := reflect.ValueOf(v)
+	ptr := unsafe.Pointer(val.Pointer()) // #nosec
+
+	data := toBytes(ptr, val.Elem().Type())
 	_, err := w.Write(data)
 	if err != nil {
 		return fmt.Errorf("Store: %v", err)
@@ -27,22 +32,58 @@ func Store(w io.Writer, v interface{}) error {
 
 // toBytes returns a byte slice pointing to the memory segment
 // where v points to. v has to be a pointer.
-func toBytes(v interface{}) []byte {
-	// convert the value to a byte slice
-	val := reflect.ValueOf(v)
-	size := val.Elem().Type().Size()
-	ptr := unsafe.Pointer(val.Pointer()) // #nosec
-	data := (*(*[1<<31 - 1]byte)(ptr))[:size]
+func toBytes(ptr unsafe.Pointer, t reflect.Type) []byte {
+	data := bytesAt(ptr, t.Size())
 
-	// pointers are not yet supported
+	ptrs := getPointers(data, ptr, t)
+	_ = ptrs
 
 	return data
+}
+
+func bytesAt(p unsafe.Pointer, size uintptr) []byte {
+	return (*(*[1<<31 - 1]byte)(p))[:size]
+}
+
+type nestedPtr struct {
+	ptr unsafe.Pointer
+	t   reflect.Type
 }
 
 // getPointers returns a map with offsets within v. These offsets
 // are pointers pointing to some memory location.
 // The returned map associates the memory offsets of the pointers
 // with their destinations.
-func getPointers(v interface{}) map[uintptr]unsafe.Pointer {
-	panic("TODO")
+func getPointers(data []byte, ptr unsafe.Pointer, t reflect.Type) map[uintptr]nestedPtr {
+	ptrs := make(map[uintptr]nestedPtr)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Type.Kind() == reflect.Ptr {
+			tField := t.Field(i)
+			offset := tField.Offset
+
+			var encPtr uintptr
+			const uintptrsize = unsafe.Sizeof(encPtr)
+			ptrBytes := data[offset : offset+uintptrsize]
+			switch unsafe.Sizeof(encPtr) {
+			case 4:
+				ptrs[offset] = nestedPtr{
+					// this use of unsafe.Pointer should be correct
+					ptr: unsafe.Pointer(uintptr(endian.Endian.Uint32(ptrBytes))), // #nosec
+					t:   tField.Type,
+				}
+			case 8:
+				ptrs[offset] = nestedPtr{
+					// this use of unsafe.Pointer should be correct
+					ptr: unsafe.Pointer(uintptr(endian.Endian.Uint64(ptrBytes))), // #nosec
+					t:   tField.Type,
+				}
+			default:
+				panic("Unknown uintptr size: " + string(uintptrsize))
+			}
+		}
+	}
+
+	return ptrs
 }
