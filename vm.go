b3f71446ea7ef0bdbe2f06900c4018bca54f68d5
Tim Satke <tim.satke@gmail.com>
2019-09-01 20:03:26 +0200

    Add support for evaluating multiple readers

--- vm.go
+++ vm.go
@@ -1,11 +1,16 @@
 package gojis
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
 // Type represents the ECMAScript language types.
 type Type uint8
 
-// Available types. TypeUnknown must not be used.
-// If it appears, this indicates, that somewhere the type was not set
-// (it is the default value for Type).
+// Available types. TypeUnknown must not be used. If it appears, this indicates,
+// that somewhere the type was not set (it is the default value for Type).
 const (
 	TypeUnknown = iota
 	TypeUndefined
@@ -17,8 +22,8 @@ const (
 	TypeObject
 )
 
-// VM represents an instance of the GojisVM.
-// It can be used to evaluate ECMAScript code.
+// VM represents an instance of the GojisVM. It can be used to evaluate
+// ECMAScript code.
 type VM struct {
 	Object // the global object
 }
@@ -28,17 +33,27 @@ func NewVM() *VM {
 	panic("TODO")
 }
 
-// Eval evaluates the given ECMAScript code, and returns an Object, representing the
-// result of the evaluation. The result may be Null or Undefined.
+// Eval evaluates the given ECMAScript code, and returns an Object, representing
+// the result of the evaluation. The result may be Null or Undefined.
 //
-// Internally, this function directly delegates to the method 'eval' that is specified
-// by the ECMAScript language specification.
+// Internally, this function directly delegates to the method 'eval' that is
+// specified by the ECMAScript language specification.
 func (vm *VM) Eval(script string) (Object, error) {
 	return vm.Lookup("eval").CallWithArgs(script)
 }
 
-// SetConsole is used to change the console of the VM.
-// Calls like 'console.log' will be written to the given Console.
+// EvalReaders reads all bytes from the given readers, concatenates them,
+// converts them to a string, and calls Eval on this VM object.
+func (vm *VM) EvalReaders(r ...io.Reader) (Object, error) {
+	data, err := ioutil.ReadAll(io.MultiReader(r...))
+	if err != nil {
+		return nil, fmt.Errorf("read all: %v", err)
+	}
+	return vm.Eval(string(data))
+}
+
+// SetConsole is used to change the console of the VM. Calls like 'console.log'
+// will be written to the given Console.
 func (vm *VM) SetConsole(console Console) {
 	vm.Object.SetObject("console", console)
 }
