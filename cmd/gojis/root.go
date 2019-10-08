001452ae6c604e20c02fb3b62036e1dd9fcfb2ba
Tim Satke <tsatke@deloitte.com>
2019-10-08 14:16:53 +0200

    Implemented CLI flag to enable CPU profiling

---
+++ cmd/gojis/root.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/gojisvm/gojis"
+	"github.com/pkg/profile"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	AppName = "gojis"
+
+	Profiling = "profiling"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   AppName,
+		Short: "An engine to execute ECMAScript",
+		Run: func(_ *cobra.Command, args []string) {
+			if viper.GetBool(Profiling) {
+				p := profile.Start(profile.CPUProfile, profile.ProfilePath("."))
+				defer p.Stop()
+			}
+
+			vm := gojis.NewVM()
+			_ = vm
+		},
+	}
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().BoolP(Profiling, "p", false, "Enable to write a CPU profile to \".\"")
+
+	viper.BindPFlag(Profiling, rootCmd.PersistentFlags().Lookup(Profiling))
+}
+
+func initConfig() {
+	viper.AutomaticEnv()
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.12
 
 require (
 	github.com/antlr/antlr4 v0.0.0-20190503160947-7c334b114c20
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/pkg/profile v1.3.0
 	github.com/rs/zerolog v1.15.1-0.20190828215812-06599535fa15
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.4.0
 	github.com/tinylib/msgp v1.1.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
