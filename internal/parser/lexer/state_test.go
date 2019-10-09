bbcbbedcaaaf81f3e024af6f0298c4ba548378a3
Tim Satke <tim.satke@gmail.com>
2019-10-09 20:11:44 +0200

    Implemented more punctuator tests

--- internal/parser/lexer/state_test.go
+++ internal/parser/lexer/state_test.go
@@ -14,11 +14,23 @@ func _punctuators() TokenSequenceTests {
 	return TokenSequenceTests{
 		name: "punctuator token sequence tests",
 		tests: []TokenSequenceTest{
+			// >
 			{">", []token.Type{token.GreaterThan}},
 			{">>", []token.Type{token.RightShift}},
+			{">>=", []token.Type{token.RightShiftAssign}},
+			{">>=>", []token.Type{token.RightShiftAssign, token.GreaterThan}},
+			{">>==", []token.Type{token.RightShiftAssign, token.Assign}},
+			{">>==>", []token.Type{token.RightShiftAssign, token.Arrow}},
+			{">>==>=", []token.Type{token.RightShiftAssign, token.Arrow, token.Assign}},
+			{">>==>==", []token.Type{token.RightShiftAssign, token.Arrow, token.Equals}},
+			{">>==>===", []token.Type{token.RightShiftAssign, token.Arrow, token.StrictEquals}},
+			{">>==>===>", []token.Type{token.RightShiftAssign, token.Arrow, token.StrictEquals, token.GreaterThan}},
+			{">>===>===>", []token.Type{token.RightShiftAssign, token.Equals, token.GreaterThanOrEqualTo, token.Equals, token.GreaterThan}},
+			{">>====>===>", []token.Type{token.RightShiftAssign, token.StrictEquals, token.GreaterThanOrEqualTo, token.Equals, token.GreaterThan}},
 			{">>>", []token.Type{token.UnsignedRightShift}},
 			{">>>=", []token.Type{token.UnsignedRightShiftAssign}},
 			{">>>>", []token.Type{token.UnsignedRightShift, token.GreaterThan}},
+			// <
 		},
 	}
 }
