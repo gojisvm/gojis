5718cbe55c866ffb19f3835771dd07f527ddb818
Tim Satke <tim.satke@gmail.com>
2019-11-12 16:57:09 +0100

    Added a new flag and extracted some magic values

--- cmd/gojis/root.go
+++ cmd/gojis/root.go
@@ -14,7 +14,14 @@ const (
 
 	// Profiling is the constant key for the flag and configuration entry
 	// "profiling".
-	Profiling = "profiling"
+	Profiling          = "profiling"
+	ProfilingShorthand = "p"
+	ProfilingDefault   = false
+
+	// ProfilePath is the constant key for the flag and configuration entry
+	// "profile-path", which determines the location of the written CPU profile.
+	ProfilePath        = "profile-path"
+	ProfilePathDefault = "."
 )
 
 var (
@@ -23,7 +30,7 @@ var (
 		Short: "An engine to execute ECMAScript",
 		Run: func(_ *cobra.Command, args []string) {
 			if viper.GetBool(Profiling) {
-				p := profile.Start(profile.CPUProfile, profile.ProfilePath("."))
+				p := profile.Start(profile.CPUProfile, profile.ProfilePath(viper.GetString(ProfilePath)))
 				defer p.Stop()
 			}
 
@@ -36,9 +43,11 @@ var (
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().BoolP(Profiling, "p", false, "Enable to write a CPU profile to \".\"")
+	rootCmd.PersistentFlags().BoolP(Profiling, ProfilingShorthand, ProfilingDefault, "Enable to write a CPU profile")
+	rootCmd.PersistentFlags().String(ProfilePath, ProfilePathDefault, "Set the output path for the CPU profile. Only has an effect if profiling is enabled")
 
 	_ = viper.BindPFlag(Profiling, rootCmd.PersistentFlags().Lookup(Profiling))
+	_ = viper.BindPFlag(ProfilePath, rootCmd.PersistentFlags().Lookup(ProfilePath))
 }
 
 func initConfig() {
