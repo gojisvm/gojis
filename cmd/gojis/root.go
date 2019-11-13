e03b11b4f0cea00c774b9743ec69b493aaaceccc
Tim Satke <tim.satke@gmail.com>
2019-11-13 09:43:19 +0100

    Unexported some constants

--- cmd/gojis/root.go
+++ cmd/gojis/root.go
@@ -12,16 +12,16 @@ const (
 	// in the help text.
 	AppName = "gojis"
 
-	// Profiling is the constant key for the flag and configuration entry
+	// profiling is the constant key for the flag and configuration entry
 	// "profiling".
-	Profiling          = "profiling"
-	ProfilingShorthand = "p"
-	ProfilingDefault   = false
+	profiling          = "profiling"
+	profilingShorthand = "p"
+	profilingDefault   = false
 
-	// ProfilePath is the constant key for the flag and configuration entry
+	// profilePath is the constant key for the flag and configuration entry
 	// "profile-path", which determines the location of the written CPU profile.
-	ProfilePath        = "profile-path"
-	ProfilePathDefault = "."
+	profilePath        = "profile-path"
+	profilePathDefault = "."
 )
 
 var (
@@ -29,8 +29,8 @@ var (
 		Use:   AppName,
 		Short: "An engine to execute ECMAScript",
 		Run: func(_ *cobra.Command, args []string) {
-			if viper.GetBool(Profiling) {
-				p := profile.Start(profile.CPUProfile, profile.ProfilePath(viper.GetString(ProfilePath)))
+			if viper.GetBool(profiling) {
+				p := profile.Start(profile.CPUProfile, profile.ProfilePath(viper.GetString(profilePath)))
 				defer p.Stop()
 			}
 
@@ -43,11 +43,11 @@ var (
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().BoolP(Profiling, ProfilingShorthand, ProfilingDefault, "Enable to write a CPU profile")
-	rootCmd.PersistentFlags().String(ProfilePath, ProfilePathDefault, "Set the output path for the CPU profile. Only has an effect if profiling is enabled")
+	rootCmd.PersistentFlags().BoolP(profiling, profilingShorthand, profilingDefault, "Enable to write a CPU profile")
+	rootCmd.PersistentFlags().String(profilePath, profilePathDefault, "Set the output path for the CPU profile. Only has an effect if profiling is enabled")
 
-	_ = viper.BindPFlag(Profiling, rootCmd.PersistentFlags().Lookup(Profiling))
-	_ = viper.BindPFlag(ProfilePath, rootCmd.PersistentFlags().Lookup(ProfilePath))
+	_ = viper.BindPFlag(profiling, rootCmd.PersistentFlags().Lookup(profiling))
+	_ = viper.BindPFlag(profilePath, rootCmd.PersistentFlags().Lookup(profilePath))
 }
 
 func initConfig() {
